ec898778b2f358654a4f2e091568fd0e0b6d12fe
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-05-20 15:26:29 +0300

    add procfile

--- app/main/views.py
+++ app/main/views.py
@@ -2,13 +2,13 @@ from flask import render_template,redirect,url_for,abort,request,flash
 from app.main import main
 from app.models import User,Blog,Comment,Subscriber
 from .forms import UpdateProfile,CreateBlog
-from .. import db
+from .. import db,photos
 from app.requests import get_quotes
 from flask_login import login_required,current_user
 from ..email import mail_message
 import secrets
 import os
-from PIL import Image
+
 
 @main.route('/')
 def index():
@@ -17,17 +17,12 @@ def index():
     blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page, per_page = 3)
     return render_template('index.html', quote = quotes,blogs=blogs)
 def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_filename = random_hex + f_ext
-    picture_path = os.path.join('app/static/photos', picture_filename)
-    
-    output_size = (200, 200)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-    return picture_filename
-
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        blog.picture_path = path
+        db.session.commit()
+    return form_picture
 @main.route('/profile',methods = ['POST','GET'])
 @login_required
 def profile():
--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sandra:12345@localhost/blog'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
